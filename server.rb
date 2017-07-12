ba9e1c258f716057650d872380e24330bcb3a3f5
AlB <albert.burc@gmail.com>
2017-07-12 18:58:13 -0400

    donezo

--- server.rb
+++ server.rb
@@ -12,8 +12,17 @@ get '/portfolio' do
   erb :gallery
 end
 
+get '/gallery' do
+  redirect to ('/portfolio')
+end
+
 get '/about_me' do
   @skills = ['git', 'HTML', 'CSS', 'Ruby']
   @interests = ['cats', 'art', 'music', 'films', 'coffee' ]
   erb :about
+end
+
+get '/favourites' do
+  @fav_links = ['www.google.com', 'www.reddit.com', 'www.toronto.ca']
+  erb :favourites
 end
